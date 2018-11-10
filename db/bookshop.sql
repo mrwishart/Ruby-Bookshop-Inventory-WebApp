55441fca2da1aaa6f19d8c8a2142ffe9a0941a5f
Paul Wishart <misterwishart@gmail.com>
2018-11-10 13:58:45 +0000

    Created bookshop sql file. Added authors and genres tables to DB

---
+++ db/bookshop.sql
@@ -0,0 +1,15 @@
+DROP TABLE IF EXISTS authors;
+DROP TABLE IF EXISTS genres;
+DROP TABLE IF EXISTS shopbooks;
+DROP TABLE IF EXISTS neilsenbooks;
+
+CREATE TABLE authors
+( id SERIAL8 PRIMARY KEY,
+  first_name VARCHAR(255),
+  last_name VARCHAR(255)
+);
+
+CREATE TABLE genres
+( id SERIAL8 PRIMARY KEY,
+  title VARCHAR(255) NOT NULL
+);
