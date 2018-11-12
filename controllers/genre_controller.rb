fc30eef54d056663ef5d5ec882c79c03934e09b8
Paul Wishart <misterwishart@gmail.com>
2018-11-12 15:34:03 +0000

    Implemented new/create/edit/update methods to Genre in a similar manner to authors. Tested successfully

--- controllers/genre_controller.rb
+++ controllers/genre_controller.rb
@@ -10,6 +10,19 @@ get '/genre' do
   erb(:"genre/index")
 end
 
+# NEW
+
+get '/genre/new' do
+  erb(:"genre/new")
+end
+
+# CREATE
+
+post '/genre' do
+  Genre.new(params).save
+  redirect to '/genre'
+end
+
 # SHOW
 get '/genre/:id' do
   @genre = Genre.find(params['id'])
@@ -18,4 +31,23 @@ get '/genre/:id' do
   else
   erb(:"genre/show")
   end
+end
+
+# EDIT
+
+get '/genre/:id/edit' do
+  @genre = Genre.find(params['id'])
+  if @genre.nil?
+    erb(:'404')
+  else
+    erb(:'genre/edit')
+  end
+end
+
+# UPDATE
+
+post '/genre/:id' do
+  @genre = Genre.new(params)
+  @genre.update
+  redirect to '/genre/' + @genre.id.to_s
 end
