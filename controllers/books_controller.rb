95e49ea76207736d21f2684d35fefc12c9fa14f2
Paul Wishart <misterwishart@gmail.com>
2018-11-12 21:42:01 +0000

    Further coded Book/new. Added in wholesaler pull-down colum and put whole form into table. Still need to add authors/genres

--- controllers/books_controller.rb
+++ controllers/books_controller.rb
@@ -3,14 +3,33 @@ require('sinatra/contrib/all') if development?
 also_reload('./models/*')
 
 require_relative('../models/book')
+require_relative('../models/wholesaler')
 
-#
-get '/books/:id' do
-  @book = Book.find_by_id(params['id'])
-  erb (:"books/show")
-end
+# INDEX
 
 get '/books' do
   @books = Book.all()
   erb(:"books/index")
+end
+
+# NEW
+
+get '/books/new' do
+  @wholesalers = Wholesaler.all()
+  erb(:"books/new")
+end
+
+# CREATE
+
+post '/books' do
+  Book.new(params).save
+  redirect to '/books'
+end
+
+
+# SHOW
+
+get '/books/:id' do
+  @book = Book.find_by_id(params['id'])
+  erb (:"books/show")
 end
