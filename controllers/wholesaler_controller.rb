dd6180e2f1d92d6aa94a28ae3f069414adee49e8
Paul Wishart <misterwishart@gmail.com>
2018-11-12 12:08:57 +0000

    Put wholesaler index into a table. Created wholesaler show page and linked accordingly

--- controllers/wholesaler_controller.rb
+++ controllers/wholesaler_controller.rb
@@ -4,8 +4,14 @@ also_reload('./models/*')
 
 require_relative('../models/wholesaler')
 
-#
+# INDEX
 get '/wholesaler' do
   @wholesalers = Wholesaler.all()
   erb(:"wholesaler/index")
+end
+
+#SHOW
+get '/wholesaler/:id' do
+  @wholesaler = Wholesaler.find_by_id(params['id'])
+  erb(:"wholesaler/show")
 end
