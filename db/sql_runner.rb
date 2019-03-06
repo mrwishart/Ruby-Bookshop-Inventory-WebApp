30ff11db4446ba2f6fff44111494e7a2d5625943
Paul Wishart <misterwishart@gmail.com>
2019-03-06 00:20:11 +0000

    Added PSQL functionality to Heroku

--- db/sql_runner.rb
+++ db/sql_runner.rb
@@ -4,7 +4,11 @@ class SqlRunner
 
   def self.run( sql, values = [] )
     begin
-      db = PG.connect({ dbname: 'bookshop', host: 'localhost' })
+      db = PG.connect({ dbname: 'dckhg6r2igrhnb',
+        host: 'ec2-54-75-230-41.eu-west-1.compute.amazonaws.com',
+        port: '5432',
+        user: 'mckuskmtfgrorh',
+        password: '2f2245d4d9dcc99463f33da592814425932f9b70ff47ef426af0d58aff829fe7' })
       db.prepare("query", sql)
       result = db.exec_prepared( "query", values )
     ensure
